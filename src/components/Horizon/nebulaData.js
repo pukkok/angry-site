be91530d572e87857d5f39b33e49959cfbc10610
Pukkok <krystalgggg@gmail.com>
2025-07-23 13:29:37 +0900

    [Feat] 성운용 데이터 정리

---
+++ src/components/Horizon/nebulaData.js
@@ -0,0 +1,95 @@
+import Orion from '@/assets/nebulas/Orion.jpg'
+import Crab from '@/assets/nebulas/Crab.jpg'
+import Eagle from '@/assets/nebulas/Eagle.jpg'
+import Lagoon from '@/assets/nebulas/Lagoon.png'
+import Horsehead from '@/assets/nebulas/Horsehead.jpg'
+import Rosette from '@/assets/nebulas/Rosette.jpg'
+import Helix from '@/assets/nebulas/Helix.jpg'
+import Carina from '@/assets/nebulas/Carina.jpg'
+import Veil from '@/assets/nebulas/Veil.jpg'
+import Trifid from '@/assets/nebulas/Trifid.jpg'
+
+const nebulaData = [
+  {
+    id: 'orion',
+    name: 'Orion Nebula',
+    emoji: '🌌',
+    short: '별의 요람',
+    description: '지구에서 가장 밝고 가까운 성운. 별들이 태어나는 곳.',
+    image: Orion,
+  },
+  {
+    id: 'crab',
+    name: 'Crab Nebula',
+    emoji: '🦀',
+    short: '초신성 잔해',
+    description: '1054년 초신성 폭발의 흔적. 현재도 팽창 중.',
+    image: Crab,
+  },
+  {
+    id: 'eagle',
+    name: 'Eagle Nebula',
+    emoji: '🦅',
+    short: '창조의 기둥',
+    description: "'창조의 기둥(Pillars of Creation)' 사진으로 유명.",
+    image: Eagle,
+  },
+  {
+    id: 'lagoon',
+    name: 'Lagoon Nebula',
+    emoji: '🌫',
+    short: '분홍빛 대형 성운',
+    description: '가스와 먼지로 이루어진 거대한 별 생성 지대.',
+    image: Lagoon,
+  },
+  {
+    id: 'horsehead',
+    name: 'Horsehead Nebula',
+    emoji: '🐎',
+    short: '상징적인 실루엣',
+    description: '말 머리 모양의 독특한 어두운 성운.',
+    image: Horsehead,
+  },
+  {
+    id: 'rosette',
+    name: 'Rosette Nebula',
+    emoji: '💜',
+    short: '장미처럼 생긴 성운',
+    description: '붉고 둥근 구조가 특징. 산개성단이 중심에 있음.',
+    image: Rosette,
+  },
+  {
+    id: 'helix',
+    name: 'Helix Nebula',
+    emoji: '🔵',
+    short: '우주의 눈',
+    description: '백색왜성 주위의 가스층. 마치 눈처럼 생김.',
+    image: Helix,
+  },
+  {
+    id: 'carina',
+    name: 'Carina Nebula',
+    emoji: '🔶',
+    short: '대형 별 형성 지역',
+    description: '매우 활동적인 별 생성 영역, 아름다운 구조.',
+    image: Carina,
+  },
+  {
+    id: 'veil',
+    name: 'Veil Nebula',
+    emoji: '💥',
+    short: '초신성 파편',
+    description: '얇고 섬세한 실타래 형태. 폭발의 흔적.',
+    image: Veil,
+  },
+  {
+    id: 'trifid',
+    name: 'Trifid Nebula',
+    emoji: '🌀',
+    short: '삼중 구조',
+    description: '방출, 반사, 암흑 성운이 함께 있는 복합체.',
+    image: Trifid,
+  },
+]
+
+export default nebulaData

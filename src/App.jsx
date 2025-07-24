02eb99e51cc78fab22cf2549e6fbebbae299f752
Pukkok <krystalgggg@gmail.com>
2025-07-24 13:01:28 +0900

    [Feat] 하프 섹션 작업 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import HalfSection from "./components/Half"
+import TestSection from "./components/Half/Test"
 import HeroSection from "./components/Hero"
 import HorizonSection from "./components/Horizon"
+import SlateSection from "./components/Slate"
 
 const App = () => {
 
   return (
     <div className="">
       <HeroSection />
+      <SlateSection>NEBULA</SlateSection>
       <HorizonSection />
+      <SlateSection>CONSTELLATION</SlateSection>
       <HalfSection />
+      <SlateSection>something wrong</SlateSection>
+      <TestSection />
     </div>
   )
 }

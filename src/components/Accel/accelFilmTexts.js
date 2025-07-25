cf03880ba63b18f1a95a50ee21e0b37b4133a5fb
Pukkok <krystalgggg@gmail.com>
2025-07-25 14:04:08 +0900

    [Feat] 필름 데이터 생성

---
+++ src/components/Accel/accelFilmTexts.js
@@ -0,0 +1,37 @@
+export const constellationTexts = [
+  'Orion - 오리온',
+  'Cassiopeia - 카시오페아',
+  'Ursa Major - 큰곰',
+  'Ursa Minor - 작은곰',
+  'Lyra - 거문고',
+  'Cygnus - 백조',
+  'Scorpius - 전갈',
+  'Leo - 사자',
+  'Gemini - 쌍둥이',
+  'Taurus - 황소'
+]
+
+export const symbolTexts = [
+  'Hunter - 사냥꾼',
+  'Queen - 여왕',
+  'Bear - 곰',
+  'Harp - 하프',
+  'Swan - 백조',
+  'Scorpion - 전갈',
+  'Lion - 사자',
+  'Twins - 쌍둥이',
+  'Bull - 황소'
+]
+
+export const spaceTermTexts = [
+  'Nebula - 성운',
+  'Galaxy - 은하',
+  'Cosmos - 코스모스',
+  'Meteor - 유성',
+  'Comet - 혜성',
+  'Satellite - 위성',
+  'Black Hole - 블랙홀',
+  'Quasar - 퀘이사',
+  'Asteroid - 소행성',
+  'Wormhole - 웜홀'
+]

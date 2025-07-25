8838126fde32e36332267a69787d44c5794b2bbb
Pukkok <krystalgggg@gmail.com>
2025-07-26 01:08:57 +0900

    [Feat] 패럴랙스 넥스트 문구 추가

--- src/components/Accel/index.jsx
+++ src/components/Accel/index.jsx
@@ -1,16 +1,25 @@
+import NextDescription from "../NextDescription"
 import TextFilm from "./TextFilm"
 import {constellationTexts, symbolTexts, spaceTermTexts } from "./accelFilmTexts"
 
 const AccelSection = () => {
 
   return (
-    <section className="w-screen h-[200vh] overflow-x-hidden bg-black">
+    <section className="w-screen  overflow-x-hidden bg-black">
       <TextFilm texts={constellationTexts} direction={-1} tilt={-3} selectType="a"/>
       <TextFilm texts={symbolTexts} direction={1} selectType="b"/>
       <TextFilm texts={spaceTermTexts} direction={-1} tilt={-3} selectType="c"/>
       <TextFilm texts={constellationTexts} direction={1} selectType="d"/>
       <TextFilm texts={symbolTexts} direction={-1} tilt={-3} selectType="e"/>
       <TextFilm texts={spaceTermTexts} direction={1} selectType="f"/>
+
+      <div className="px-20 mt-40 text-white">
+        <NextDescription 
+          first={'PARALLAX'}
+          second={'DIFFERENT SPEED'}
+          description={'각자 다른 속도로 움직입니다'}
+        />
+      </div>
     </section>
   )
 }
